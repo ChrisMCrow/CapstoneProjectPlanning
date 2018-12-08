3a4743fd2550dace83222d82d20a1e3093e3d61c
Chris Crow <chrismcrow@gmail.com>
2018-12-07 16:03:54 -0800

    add Navbar component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,25 +2,28 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import SignIn from './SignIn';
-import Home from './Home';
-import Friends from './Friends';
-import Shows from './Shows';
+import Home from './HomePage';
+import Friends from './FriendsPage';
+import Shows from './ShowsPage';
 
 function App() {
   return (
     <div>
       <style global jsx>{`
-        color: 
-        background-color: black;
-
+        body {
+          color: white;
+          background-color: black;
+        }
       `}</style>
       <Navbar/>
-      <Switch>
-        <Route exact path='/' component={SignIn} />
-        <Route path='/home' component={Home} />
-        <Route path='/friends' component={Friends} />
-        <Route path='/shows' component={Shows} />
-      </Switch>
+      <div className='container'>
+        <Switch>
+          <Route exact path='/' component={SignIn} />
+          <Route path='/home' component={Home} />
+          <Route path='/friends' component={Friends} />
+          <Route path='/shows' component={Shows} />
+        </Switch>
+      </div>
     </div>
   );
 }
